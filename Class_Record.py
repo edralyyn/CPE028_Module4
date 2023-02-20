8a4b55fa14a06e40bc403216fab46188ed9c09f5
edralyyn <qerperalta@tip.edu.ph>
2023-02-20 10:30:14 +0000

    Add Class_Record.py

---
+++ Class_Record.py
@@ -0,0 +1,11 @@
+import Student
+import Grades_Class
+
+p1 = Student.Student("Edralyn Peralta", 22)
+p2 = Grades_Class.Grades_Class("CPE028", 3, 1)
+print(p1)
+print("Name:", p1.name)
+print("Age:", p1.age)
+print("Course:", p2.Course_Code)
+print("Units:", p2.Course_Units)
+print("Grade:", p2.Course_Grade)
